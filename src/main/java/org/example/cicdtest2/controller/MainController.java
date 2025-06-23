9c815a18da57b0e832270bbb255333242510e9e4
yubeen777 <panda7715@naver.com>
2025-06-23 16:41:49 +0900

    reupload

--- src/main/java/org/example/cicdtest2/controller/MainController.java
+++ src/main/java/org/example/cicdtest2/controller/MainController.java
@@ -1,4 +1,12 @@
 package org.example.cicdtest2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+  @GetMapping
+  public String hello() {
+    return "This is cicd test2";
+  }
 }
