4a312655e08bdb88e825341581d227f6327282dc
yubeen777 <panda7715@naver.com>
2025-06-24 11:37:53 +0900

    autodeployement test1

---
+++ src/test/java/org/example/cicdtest2/controller/MainControllerTest.java
@@ -0,0 +1,16 @@
+package org.example.cicdtest2.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MainControllerTest {
+
+  @Test
+  void hello() {
+    Assertions.assertThat(true).isTrue();
+//    throw  new RuntimeException("강제 오류 발생");
+  }
+}
