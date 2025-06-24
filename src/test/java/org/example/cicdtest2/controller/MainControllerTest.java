21845865aa5e4b5001ca0b9c32b59e881f5528be
yubeen777 <panda7715@naver.com>
2025-06-24 11:39:31 +0900

    autodeployement erorr test1

--- src/test/java/org/example/cicdtest2/controller/MainControllerTest.java
+++ src/test/java/org/example/cicdtest2/controller/MainControllerTest.java
@@ -1,6 +1,5 @@
 package org.example.cicdtest2.controller;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +9,7 @@ class MainControllerTest {
 
   @Test
   void hello() {
-    Assertions.assertThat(true).isTrue();
-//    throw  new RuntimeException("강제 오류 발생");
+//    Assertions.assertThat(true).isTrue();
+    throw  new RuntimeException("강제 오류 발생");
   }
 }
